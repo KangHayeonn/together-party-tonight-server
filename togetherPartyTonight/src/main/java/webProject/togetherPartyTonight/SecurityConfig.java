def7b429ae6184c13fbd76a9d74bef0ce1e7b14d
Choi-chunsam <cdw5713@naver.com>
2023-06-12 18:14:09 +0900

    [TEST] : ci/cd테스트 도메인

--- togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/SecurityConfig.java
+++ togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/SecurityConfig.java
@@ -1,2 +1,17 @@
-package webProject.togetherPartyTonight;public class SecurityConfig {
+package webProject.togetherPartyTonight;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin().disable();
+        http.authorizeRequests().anyRequest().permitAll();
+
+        return http.build();
+    }
 }
--- togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/TestController.java
+++ togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/TestController.java
@@ -1,6 +1,8 @@
 package webProject.togetherPartyTonight;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/")
-    public String test(){
-        return "반갑다.";
+    public ResponseEntity<HttpStatus> test(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
