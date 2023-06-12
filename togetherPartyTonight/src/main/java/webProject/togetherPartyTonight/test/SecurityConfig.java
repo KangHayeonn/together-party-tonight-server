e8c69a69ace63758d47d51b734595710e116a229
Choi-chunsam <cdw5713@naver.com>
2023-06-12 19:08:37 +0900

    [TEST] : ci/cd 마지막 테스트

--- togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/test/SecurityConfig.java
+++ togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/test/SecurityConfig.java
@@ -1,5 +1,6 @@
 package webProject.togetherPartyTonight.test;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -7,7 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin().disable()
                 .httpBasic().disable()
