189c74cba5ea3aeb7e99f922616bc719de5c8360
GyeongEun-Kim <kekim20@naver.com>
2023-08-01 17:47:04 +0900

    [ADD] : 모임 추가,수정,삭제시 태그 테이블에 반영되도록 수정

--- togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/domain/search/entity/Tag.java
+++ togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/domain/search/entity/Tag.java
@@ -12,14 +12,16 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "tag")
+@Table(name = "tag", indexes = {
+        @Index(name = "tag_count_idx", columnList = "tag_count")
+})
 public class Tag extends BaseEntity {
     @Id
     @Column(name = "tag_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long tagId;
 
-    @Column(name="tag_name", nullable = false)
+    @Column(name="tag_name", nullable = false, unique = true)
     private String tagName;
 
     @Column(name="tag_count", nullable = false)
--- togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/domain/search/repository/TagRepository.java
+++ togetherPartyTonight/src/main/java/webProject/togetherPartyTonight/domain/search/repository/TagRepository.java
@@ -1,6 +1,9 @@
 package webProject.togetherPartyTonight.domain.search.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import webProject.togetherPartyTonight.domain.search.entity.Tag;
 
@@ -10,4 +13,14 @@ import java.util.List;
 public interface TagRepository extends JpaRepository<Tag,Long> {
 
     List<Tag> findTop12ByOrderByTagCountDesc ();
+
+    @Modifying
+    @Query(nativeQuery = true, value = "INSERT INTO tag (created_date, modified_date, tag_name, tag_count) " +
+            "VALUES (NOW(), NOW(), :tagName, 1) " +
+            "ON DUPLICATE KEY UPDATE tag_count = tag_count + 1;")
+    void saveTagNameAndTagCount(@Param(value = "tagName")String tagName);
+
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE tag SET tag_count= tag_count -1 WHERE tag_name = :tagName")
+    void deleteTagCount(@Param(value = "tagName")String tagName);
 }
